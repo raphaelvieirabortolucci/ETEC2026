a2040f58ae77b976990272b94645794d107fd5d6
raphaelvieirabortolucci <raphaelvieirabortolucci@gmail.com>
2026-09-21 16:32:00 -0300

    first commit

---
+++ IW-I/geralAjax/explicacoes/js/script.js
@@ -0,0 +1,25 @@
+// 22. Exercício para você praticar
+
+// Antes de partirmos para a atividade do seu professor, eu recomendo você fazer este pequeno exercício:
+
+// HTML
+// <button id="btn">Buscar usuário</button>
+
+// <h2 id="nome"></h2>
+// <p id="email"></p>
+// Objetivo
+
+// Quando clicar no botão:
+
+// Fazer fetch() para:
+// https://jsonplaceholder.typicode.com/users/1
+// Usar async.
+// Usar await.
+// Usar try.
+// Usar catch.
+// Converter a resposta para JSON.
+// Mostrar:
+// nome do usuário
+// email do usuário
+
+
